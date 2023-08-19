0d2f7922ffe2017e35da90a018630b1220a60ffb
Krishna Nand Dubey <krishnanand025@gmail.com>
2023-08-19 19:43:42 +0530

    changes

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 const cookieParser= require("cookie-parser");
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); 
-    next();
-    });
 app.use(cookieParser());
 app.use(express.json());
 app.use(require('./src/routers/auth'))
